130fddc6deebb297b23d1759b7c50d467d6f8786
Sahil Jambhulkar <138851848+SahilJam@users.noreply.github.com>
2023-08-02 20:20:29 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,34 @@
+
+let allMusic = [
+  {
+    name: "Calm Down",
+    artist: "Rema, Selena Gomez",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Perfect",
+    artist: "Ed Sheeran",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Sorry",
+    artist: "Justin Bieber",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Roar",
+    artist: "Katy Perry",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Maan Meri Jaan",
+    artist: "King",
+    img: "music-5",
+    src: "music-5"
+  },
+  
+];
